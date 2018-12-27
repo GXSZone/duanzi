fcd4f682d7f83fb6c0e4b54fec27da825e33bd92
lwqiu <qlw2030@163.com>
2018-12-27 14:53:00 +0800

    H5直接返回对象行不通,只能是string

--- app/src/main/java/com/caotu/duanzhi/other/AndroidInterface.java
+++ app/src/main/java/com/caotu/duanzhi/other/AndroidInterface.java
@@ -3,11 +3,22 @@ package com.caotu.duanzhi.other;
 import android.webkit.JavascriptInterface;
 
 import com.caotu.duanzhi.module.other.WebActivity;
+import com.caotu.duanzhi.utils.MySpUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AndroidInterface {
 
     @JavascriptInterface
     public String callappforkey() {
-        return WebActivity.H5_KEY;
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("userid", MySpUtils.getMyId());
+            jsonObject.put("key", WebActivity.H5_KEY);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
     }
 }
