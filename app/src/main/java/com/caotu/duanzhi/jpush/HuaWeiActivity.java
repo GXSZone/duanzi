73641d6ded16d65a419640cb3846464ac26ea430
lwqiu <qlw2030@163.com>
2018-12-28 18:50:42 +0800

    1.添加华为推送中间页跳转 2.修改H5页面视频全屏问题

---
+++ app/src/main/java/com/caotu/duanzhi/jpush/HuaWeiActivity.java
@@ -0,0 +1,158 @@
+package com.caotu.duanzhi.jpush;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.caotu.duanzhi.Http.CommonHttpRequest;
+import com.caotu.duanzhi.MyApplication;
+import com.caotu.duanzhi.R;
+import com.caotu.duanzhi.config.HttpApi;
+import com.caotu.duanzhi.module.base.BaseActivity;
+import com.caotu.duanzhi.module.home.ContentDetailActivity;
+import com.caotu.duanzhi.module.home.MainActivity;
+import com.caotu.duanzhi.module.other.WebActivity;
+import com.google.gson.Gson;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class HuaWeiActivity extends BaseActivity {
+    /**
+     * 消息Id
+     **/
+    private static final String KEY_MSGID = "msg_id";
+    /**
+     * 该通知的下发通道
+     **/
+    private static final String KEY_WHICH_PUSH_SDK = "rom_type";
+    /**
+     * 通知标题
+     **/
+    private static final String KEY_TITLE = "n_title";
+    /**
+     * 通知内容
+     **/
+    private static final String KEY_CONTENT = "n_content";
+    /**
+     * 通知附加字段
+     **/
+    private static final String KEY_EXTRAS = "n_extras";
+    private static final String TAG = "huaweiActivity";
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleOpenClick();
+    }
+
+    @Override
+    protected void initView() {
+        textView = findViewById(R.id.text_huawei);
+    }
+
+    @Override
+    protected int getLayoutView() {
+        return R.layout.activity_hua_wei;
+    }
+
+
+    private void handleOpenClick() {
+        if (getIntent().getData() == null) return;
+        String data = getIntent().getData().toString();
+        if (TextUtils.isEmpty(data)) return;
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            String msgId = jsonObject.optString(KEY_MSGID);
+            byte whichPushSDK = (byte) jsonObject.optInt(KEY_WHICH_PUSH_SDK);
+            String title = jsonObject.optString(KEY_TITLE);
+            String content = jsonObject.optString(KEY_CONTENT);
+            String extras = jsonObject.optString(KEY_EXTRAS);
+
+            JpushBean jpushBean = new Gson().fromJson(extras, JpushBean.class);
+            String type = jpushBean.getType();
+            String url = jpushBean.getUrl();
+            String pushid = jpushBean.getPushid();
+            Intent openIntent = new Intent();
+            if (!TextUtils.isEmpty(pushid)) {
+                //接口请求
+                notifyInterface(pushid);
+            }
+            if (TextUtils.isEmpty(url)) {
+                return;
+            }
+            switch (type) {
+                case "1":
+                    //内容审核和举报消息
+                    openIntent.setClass(this, ContentDetailActivity.class);
+                    openIntent.putExtra("contentId", url);
+                    break;
+                case "4":
+                    //H5
+                    openIntent.setClass(this, WebActivity.class);
+                    openIntent.putExtra(WebActivity.KEY_TITLE, "web");
+                    openIntent.putExtra(WebActivity.KEY_URL, url);
+                    break;
+                default:
+                    openIntent.setClass(this, MainActivity.class);
+                    break;
+            }
+            openIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (MyApplication.getInstance().getLastSecondActivity() == null) {
+                //判断是否APP还还活着的逻辑
+                Intent[] intents = new Intent[2];
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intents[0] = intent;
+                intents[1] = openIntent;
+                startActivities(intents);
+                finish();
+            } else {
+                startActivity(openIntent);
+                finish();
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append("msgId:");
+            sb.append(String.valueOf(msgId));
+            sb.append("\n");
+            sb.append("title:");
+            sb.append(String.valueOf(title));
+            sb.append("\n");
+            sb.append("content:");
+            sb.append(String.valueOf(content));
+            sb.append("\n");
+            sb.append("extras:");
+            sb.append(String.valueOf(extras));
+            sb.append("\n");
+            sb.append("platform:");
+            Log.i(TAG, "handleOpenClick: " + sb.toString());
+            //上报点击事件
+            JPushInterface.reportNotificationOpened(this, msgId, whichPushSDK);
+        } catch (JSONException e) {
+            Log.w(TAG, "parse notification error");
+        }
+    }
+
+    private void notifyInterface(String pushId) {
+        Map<String, String> map = CommonHttpRequest.getInstance().getHashMapParams();
+        map.put("pushid", pushId);
+        OkGo.<String>post(HttpApi.PUSH_OPEN)
+                .upJson(new JSONObject(map))
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+
+                    }
+                });
+    }
+}
