9880a98e8875cc979e4cb94c990561a2e643106d
lwqiu <qlw2030@163.com>
2019-01-03 17:16:27 +0800

    修改视频下载代码

--- app/src/main/java/com/caotu/duanzhi/view/dialog/ShareDialog.java
+++ app/src/main/java/com/caotu/duanzhi/view/dialog/ShareDialog.java
@@ -8,16 +8,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -42,8 +37,6 @@ import com.lzy.okgo.model.Response;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 
 /**
  * @author mac
@@ -51,41 +44,12 @@ import java.lang.reflect.Field;
  * @describe 分享弹窗
  * 分享弹窗逻辑是:先在外部判断好是否显示收藏和视频下载按钮的显示,弹窗内部只处理赋值分享的平台,真正唤起三方分享在sharehelp里实现
  */
-public class ShareDialog extends BottomSheetDialogFragment implements View.OnClickListener {
-    private static final String TAG = "download";
+public class ShareDialog extends BaseDialogFragment implements View.OnClickListener {
 
-    /**
-     * 微信好友
-     */
-    private TextView mShareWeixin;
-    /**
-     * 朋友圈
-     */
-    private TextView mShareFriend;
-    /**
-     * QQ好友
-     */
-    private TextView mShareQq;
-    /**
-     * QQ空间
-     */
-    private TextView mShareQqSpace;
-    /**
-     * 微博
-     */
-    private TextView mShareWeibo;
-    /**
-     * 收藏
-     */
-    private TextView mShareCollection;
     /**
      * 保存至相册
      */
     private TextView mShareDownloadVideo;
-    /**
-     * 取消
-     */
-    private TextView mTvClickCancel;
     //分享内容的对象
     private WebShareBean bean;
     //静态变量才能保证不随fragment的销毁而制空
@@ -102,36 +66,32 @@ public class ShareDialog extends BottomSheetDialogFragment implements View.OnCli
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getDate();
-        View inflate = inflater.inflate(R.layout.layout_share_dialog, container, false);
-        initView(inflate);
-        return inflate;
-    }
-
-    private void getDate() {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             bean = getArguments().getParcelable("bean");
         }
     }
 
-    private void initView(View inflate) {
-        mShareWeixin = (TextView) inflate.findViewById(R.id.share_weixin);
-        mShareWeixin.setOnClickListener(this);
-        mShareFriend = (TextView) inflate.findViewById(R.id.share_friend);
-        mShareFriend.setOnClickListener(this);
-        mShareQq = (TextView) inflate.findViewById(R.id.share_qq);
-        mShareQq.setOnClickListener(this);
-        mShareQqSpace = (TextView) inflate.findViewById(R.id.share_qq_space);
-        mShareQqSpace.setOnClickListener(this);
-        mShareWeibo = (TextView) inflate.findViewById(R.id.share_weibo);
-        mShareWeibo.setOnClickListener(this);
-        mShareCollection = (TextView) inflate.findViewById(R.id.share_collection);
+    @Override
+    public int getLayout() {
+        return R.layout.layout_share_dialog;
+    }
+
+    @Override
+    protected void initView(View inflate) {
+        inflate.findViewById(R.id.share_weixin).setOnClickListener(this);
+        inflate.findViewById(R.id.share_friend).setOnClickListener(this);
+        inflate.findViewById(R.id.share_qq).setOnClickListener(this);
+        inflate.findViewById(R.id.share_qq_space).setOnClickListener(this);
+        inflate.findViewById(R.id.share_weibo).setOnClickListener(this);
+        inflate.findViewById(R.id.tv_click_cancel).setOnClickListener(this);
+
+        TextView mShareCollection = (TextView) inflate.findViewById(R.id.share_collection);
         mShareCollection.setOnClickListener(this);
         mShareDownloadVideo = (TextView) inflate.findViewById(R.id.share_download_video);
         mShareDownloadVideo.setOnClickListener(this);
-        mTvClickCancel = (TextView) inflate.findViewById(R.id.tv_click_cancel);
-        mTvClickCancel.setOnClickListener(this);
+
         //设置背景透明，才能显示出layout中诸如圆角的布局，否则会有白色底（框）
         setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.TransparentBottomSheetStyle);
         //只有内容列表和内容详情的分享视频才有下载
@@ -148,8 +108,6 @@ public class ShareDialog extends BottomSheetDialogFragment implements View.OnCli
         } else if (mShareDownloadVideo.getVisibility() == View.GONE || mShareCollection.getVisibility() == View.GONE) {
             inflate.findViewById(R.id.space).setLayoutParams(new LinearLayout.LayoutParams(0, 1, 2));
         }
-
-
     }
 
     @Override
@@ -224,7 +182,6 @@ public class ShareDialog extends BottomSheetDialogFragment implements View.OnCli
                             if (TextUtils.isEmpty(bean.VideoUrl)) return;
                             isDownLoad = true;
                             downLoadVideoUrl = bean.VideoUrl;
-                            Log.i("bianliang", "onClick: " + downLoadVideoUrl);
                             // TODO: 2018/11/28 这块是开启服务的形式
 //                            Intent intent = new Intent(MyApplication.getInstance().getRunningActivity(), WaterMarkServices.class);
 //                            intent.putExtra(WaterMarkServices.KEY_URL, bean.VideoUrl);
@@ -241,7 +198,7 @@ public class ShareDialog extends BottomSheetDialogFragment implements View.OnCli
                                         @Override
                                         public void onSuccess(Response<File> response) {
                                             downLoadVideoFile = response.body();
-                                            Log.i(TAG, "下载完成开始加水印");
+//                                            Log.i(TAG, "下载完成开始加水印");
                                             addWaterMark();
                                             mShareDownloadVideo.setEnabled(true);
                                         }
@@ -280,7 +237,7 @@ public class ShareDialog extends BottomSheetDialogFragment implements View.OnCli
                 mEditor.setOnProgessListener(new onVideoEditorProgressListener() {
                     @Override
                     public void onProgress(VideoEditor v, int percent) {
-                        Log.i(TAG, "加水印进度 onProgress: " + String.valueOf(percent) + "%");
+//                        Log.i(TAG, "加水印进度 onProgress: " + String.valueOf(percent) + "%");
                         if (percent == 100) {
                             //删除原先的
                             LanSongFileUtil.deleteDir(downLoadVideoFile);
@@ -335,34 +292,6 @@ public class ShareDialog extends BottomSheetDialogFragment implements View.OnCli
         localContentValues.put("_data", paramFile.getAbsolutePath());
         localContentValues.put("_size", Long.valueOf(paramFile.length()));
         return localContentValues;
-    }
-
-    @Override
-    public void show(FragmentManager manager, String tag) {
-        //super.show(manager, tag);
-        try {
-            Class c = Class.forName("android.support.v4.app.DialogFragment");
-            Constructor con = c.getConstructor();
-            Object obj = con.newInstance();
-            Field dismissed = c.getDeclaredField("mDismissed");
-            dismissed.setAccessible(true);
-            dismissed.set(obj, false);
-            Field shownByMe = c.getDeclaredField("mShownByMe");
-            shownByMe.setAccessible(true);
-            shownByMe.set(obj, false);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        FragmentTransaction ft = manager.beginTransaction();
-        ft.add(this, tag);
-        ft.commitAllowingStateLoss();
-
-    }
-
-    @Override
-    public void dismiss() {
-        //super.dismiss();
-        dismissAllowingStateLoss();
     }
 
     public ShareMediaCallBack listener;
