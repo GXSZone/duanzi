01aa00c432f535365b0e0b322b0007a8b04b4f3d
lwqiu <qlw2030@163.com>
2019-01-18 11:40:55 +0800

    内含段子新版1.2.0

--- app/src/main/java/com/caotu/duanzhi/config/BaseConfig.java
+++ app/src/main/java/com/caotu/duanzhi/config/BaseConfig.java
@@ -3,23 +3,23 @@ package com.caotu.duanzhi.config;
 public interface BaseConfig {
     boolean isDebug = false;//是否是Debug模式  控制log打印
 
-//    String baseApi = "http://192.168.1.114:8860/NHDZSEVER"; //测试接口
-    String baseApi = "https://api.itoutu.com:8899/NHDZSEVER"; //正式接口
+    String baseApi = "http://192.168.1.114:8860/NHDZSEVER"; //测试接口
+//    String baseApi = "https://api.itoutu.com:8899/NHDZSEVER"; //正式接口
 //    String baseApi = "http://192.168.1.111:8091/NHDZSEVER/";// 本地服务器
 //    String baseApi = "http://101.69.230.98:8860/NHDZSEVER";
     //http://101.69.230.98:8860/NHDZSEVER
 
     /*******************************内含段子***************************************/
-//    //社区公约url
-//    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact.html";
-//    //帮助反馈页面
-//    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help.html";
-//    //用户协议
-//    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol.html";
-//    String APP_NAME = "NH";
-//    //分享文本
-//    String SHARE_CONTENT_TEXT = "内含段子，内含的不只是段子";
-//    String appName = "内含段子";
+    //社区公约url
+    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact.html";
+    //帮助反馈页面
+    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help.html";
+    //用户协议
+    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol.html";
+    String APP_NAME = "NH";
+    //分享文本
+    String SHARE_CONTENT_TEXT = "内含段子，内含的不只是段子";
+    String appName = "内含段子";
 
     /**********************************内含段友****************************************/
 //    //帮助与反馈
@@ -34,16 +34,16 @@ public interface BaseConfig {
 //    String appName = "内含段友";
 
     /**********************************内含段友名称,皮皮段子icon****************************************/
-    //帮助与反馈
-    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help3.html";
-    //用户协议
-    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol3.html";
-    //社区公约
-    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact3.html";
-    String APP_NAME = "DP";
-    //分享文本
-    String SHARE_CONTENT_TEXT = "内含段友，内含的不只是段子";
-    String appName = "内含段友";
+//    //帮助与反馈
+//    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help3.html";
+//    //用户协议
+//    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol3.html";
+//    //社区公约
+//    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact3.html";
+//    String APP_NAME = "DP";
+//    //分享文本
+//    String SHARE_CONTENT_TEXT = "内含段友，内含的不只是段子";
+//    String appName = "内含段友";
 
 
     //腾讯云配置
