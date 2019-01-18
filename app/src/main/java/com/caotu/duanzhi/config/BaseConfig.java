da96f581ee66dbc659aef74a2eba325be94e2b8a
lwqiu <qlw2030@163.com>
2019-01-18 11:33:29 +0800

    添加皮皮段子icon
    (cherry picked from commit b5a80fb7cee1501cbc4d96f54f7d66e4ff540e46)

--- app/src/main/java/com/caotu/duanzhi/config/BaseConfig.java
+++ app/src/main/java/com/caotu/duanzhi/config/BaseConfig.java
@@ -1,24 +1,25 @@
 package com.caotu.duanzhi.config;
 
 public interface BaseConfig {
-    boolean isDebug = true;//是否是Debug模式  控制log打印
-    String baseApi = "http://192.168.1.114:8860/NHDZSEVER"; //测试接口
-//    String baseApi = "https://api.itoutu.com:8899/NHDZSEVER"; //正式接口
+    boolean isDebug = false;//是否是Debug模式  控制log打印
+
+//    String baseApi = "http://192.168.1.114:8860/NHDZSEVER"; //测试接口
+    String baseApi = "https://api.itoutu.com:8899/NHDZSEVER"; //正式接口
 //    String baseApi = "http://192.168.1.111:8091/NHDZSEVER/";// 本地服务器
 //    String baseApi = "http://101.69.230.98:8860/NHDZSEVER";
     //http://101.69.230.98:8860/NHDZSEVER
 
     /*******************************内含段子***************************************/
-    //社区公约url
-    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact.html";
-    //帮助反馈页面
-    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help.html";
-    //用户协议
-    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol.html";
-    String APP_NAME = "NH";
-    //分享文本
-    String SHARE_CONTENT_TEXT = "内含段子，内含的不只是段子";
-    String appName = "内含段子";
+//    //社区公约url
+//    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact.html";
+//    //帮助反馈页面
+//    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help.html";
+//    //用户协议
+//    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol.html";
+//    String APP_NAME = "NH";
+//    //分享文本
+//    String SHARE_CONTENT_TEXT = "内含段子，内含的不只是段子";
+//    String appName = "内含段子";
 
     /**********************************内含段友****************************************/
 //    //帮助与反馈
@@ -31,6 +32,18 @@ public interface BaseConfig {
 //    //分享文本
 //    String SHARE_CONTENT_TEXT = "内含段友，内含的不只是段子";
 //    String appName = "内含段友";
+
+    /**********************************内含段友名称,皮皮段子icon****************************************/
+    //帮助与反馈
+    String KEY_FEEDBACK = "https://active.diqyj.cn/apph5page_nhdz/help3.html";
+    //用户协议
+    String KEY_USER_AGREEMENT = "https://active.diqyj.cn/apph5page_nhdz/userprotocol3.html";
+    //社区公约
+    String COMMUNITY_CONVENTION = "https://active.diqyj.cn/apph5page_nhdz/pact3.html";
+    String APP_NAME = "DP";
+    //分享文本
+    String SHARE_CONTENT_TEXT = "内含段友，内含的不只是段子";
+    String appName = "内含段友";
 
 
     //腾讯云配置
@@ -45,9 +58,4 @@ public interface BaseConfig {
 
     String[] REPORTITEMS = new String[]{"广告", "低俗色情", "攻击歧视", "涉政", "血腥暴力", "赌博", "其他"};
 
-    String TYPE_VIDEO_TRANSVERSE = "1";//横视频
-    String TYPE_VIDEO_PORTRAIT = "2";//竖视频
-    String TYPE_IMAGE = "3";//图片
-    String TYPE_TEXT = "4"; //文字
-    String TYPE_WEB = "5"; //web类型
 }
