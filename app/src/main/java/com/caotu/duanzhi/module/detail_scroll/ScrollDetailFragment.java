30654935ee7fa0517e989a3a569abf1823ce2120
lwqiu <qlw2030@163.com>
2019-04-12 17:53:38 +0800

    修改详情视频自动问题

--- app/src/main/java/com/caotu/duanzhi/module/detail_scroll/ScrollDetailFragment.java
+++ app/src/main/java/com/caotu/duanzhi/module/detail_scroll/ScrollDetailFragment.java
@@ -1,12 +1,8 @@
 package com.caotu.duanzhi.module.detail_scroll;
 
-import com.caotu.duanzhi.Http.bean.MomentsDataBean;
 import com.caotu.duanzhi.MyApplication;
-import com.caotu.duanzhi.module.home.ContentDetailActivity;
 import com.caotu.duanzhi.module.home.ContentDetailFragment;
 import com.caotu.duanzhi.view.widget.StateView;
-
-import org.greenrobot.eventbus.EventBus;
 
 public class ScrollDetailFragment extends ContentDetailFragment {
 //    //只处理懒加载问题
@@ -21,15 +17,15 @@ public class ScrollDetailFragment extends ContentDetailFragment {
         if (isVisibleToUser) {
             MyApplication.getInstance().putHistory(contentId);
         }
-        if (isVisibleToUser) {
-            if (!EventBus.getDefault().isRegistered(this)) {
-                EventBus.getDefault().register(this);
-            }
-        } else {
-            if (EventBus.getDefault().isRegistered(this)) {
-                EventBus.getDefault().unregister(this);
-            }
-        }
+//        if (isVisibleToUser) {
+//            if (!EventBus.getDefault().isRegistered(this)) {
+//                EventBus.getDefault().register(this);
+//            }
+//        } else {
+//            if (EventBus.getDefault().isRegistered(this)) {
+//                EventBus.getDefault().unregister(this);
+//            }
+//        }
     }
 
     @Override
@@ -38,18 +34,18 @@ public class ScrollDetailFragment extends ContentDetailFragment {
         mStatesView.setCurrentState(StateView.STATE_CONTENT);
     }
 
-    public void changeHeaderUi(MomentsDataBean data, boolean isSkip) {
-        content = data;
-        if (viewHolder != null) {
-            //这里多加了条件
-            if (isSkip && isVisibleToUser) {
-                viewHolder.justBindCountAndState(data);
-            } else {
-                viewHolder.bindDate(data);
-            }
-        }
-        if (getActivity() != null && getActivity() instanceof ContentDetailActivity) {
-            ((ContentDetailActivity) getActivity()).setPresenter(data);
-        }
-    }
+//    public void changeHeaderUi(MomentsDataBean data, boolean isSkip) {
+//        content = data;
+//        if (viewHolder != null) {
+//            //这里多加了条件
+//            if (isSkip && isVisibleToUser) {
+//                viewHolder.justBindCountAndState(data);
+//            } else {
+//                viewHolder.bindDate(data);
+//            }
+//        }
+//        if (getActivity() != null && getActivity() instanceof ContentDetailActivity) {
+//            ((ContentDetailActivity) getActivity()).setPresenter(data);
+//        }
+//    }
 }
