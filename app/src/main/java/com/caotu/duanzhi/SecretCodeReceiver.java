5c0c98b7648b6ed840c2720de20f8d7bfa4e8a19
lwqiu <qlw2030@163.com>
2019-07-19 10:05:13 +0800

    添加暗码开启切换坏境的页面,暗号:*#*#1111#*#*

---
+++ app/src/main/java/com/caotu/duanzhi/SecretCodeReceiver.java
@@ -0,0 +1,23 @@
+package com.caotu.duanzhi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import static android.provider.Telephony.Sms.Intents.SECRET_CODE_ACTION;
+
+/**
+ * 骚气的暗码操作,拨号键盘直接切换环境
+ */
+public class SecretCodeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent != null && SECRET_CODE_ACTION.equals(intent.getAction())) {
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.setClass(context, HideActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
