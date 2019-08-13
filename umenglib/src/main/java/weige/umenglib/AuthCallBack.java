ed4f3128978b73d049bd2577b4400e09f0b2ae9a
lwqiu <qlw2030@163.com>
2019-08-13 14:15:14 +0800

    修改QQ的用户ID问题

--- umenglib/src/main/java/weige/umenglib/AuthCallBack.java
+++ umenglib/src/main/java/weige/umenglib/AuthCallBack.java
@@ -5,7 +5,7 @@ import com.umeng.socialize.bean.SHARE_MEDIA;
 
 import java.util.Map;
 
-public  class AuthCallBack implements UMAuthListener {
+public class AuthCallBack implements UMAuthListener {
     @Override
     public void onStart(SHARE_MEDIA share_media) {
         start();
@@ -15,12 +15,17 @@ public  class AuthCallBack implements UMAuthListener {
     public void onComplete(SHARE_MEDIA share_media, int i, Map<String, String> map) {
         UserAuthInfo umInfoBean = new UserAuthInfo();
         if (map != null) {
-            umInfoBean.setUid(map.get("uid"));
+            // TODO: 2019-08-13 QQ 这个有点特殊,为了跟IOS 的账号互通
+            if (share_media == SHARE_MEDIA.QQ) {
+                umInfoBean.setUid(map.get("unionid"));
+            } else {
+                umInfoBean.setUid(map.get("uid"));
+            }
             umInfoBean.setName(map.get("name"));
             umInfoBean.setIconurl(map.get("iconurl"));
             umInfoBean.setGender(map.get("gender"));
         }
-        complete(SharePlatformTranlate.umengTranlateToUser(share_media),umInfoBean);
+        complete(SharePlatformTranlate.umengTranlateToUser(share_media), umInfoBean);
     }
 
     @Override
@@ -34,19 +39,19 @@ public  class AuthCallBack implements UMAuthListener {
         cancle();
     }
 
-    public  void error(String s){
+    public void error(String s) {
 
     }
 
-    protected  void cancle(){
+    protected void cancle() {
 
     }
 
-    public  void complete(@ThirdPlatform int platform, UserAuthInfo umInfoBean){
+    public void complete(@ThirdPlatform int platform, UserAuthInfo umInfoBean) {
 
     }
 
-    public  void start(){
+    public void start() {
 
     }
 }
