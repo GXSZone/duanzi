25beba3b2b08768b485b8285ccc876ce34f0b326
lwqiu <qlw2030@163.com>
2019-11-12 16:03:10 +0800

    修改viewpager的多点触控bug

--- app/src/main/java/com/caotu/duanzhi/view/widget/SlipViewPager.java
+++ app/src/main/java/com/caotu/duanzhi/view/widget/SlipViewPager.java
@@ -10,10 +10,10 @@ import androidx.viewpager.widget.ViewPager;
 
 public class SlipViewPager extends ViewPager {
     //是否可以滑动的标志位
-    public boolean isSlipping = true;
+    public boolean isScroll = true;
 
     public void setSlipping(boolean slipping) {
-        isSlipping = slipping;
+        isScroll = slipping;
     }
 
     public SlipViewPager(@NonNull Context context) {
@@ -24,23 +24,54 @@ public class SlipViewPager extends ViewPager {
         super(context, attrs);
     }
 
+    /**
+     * 1.dispatchTouchEvent一般情况不做处理
+     * ,如果修改了默认的返回值,子孩子都无法收到事件
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);   // return true;不行
+    }
+
+    /**
+     * 是否拦截
+     * 拦截:会走到自己的onTouchEvent方法里面来
+     * 不拦截:事件传递给子孩子
+     */
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        try {
-            return isSlipping && super.onInterceptTouchEvent(ev);
-        } catch (Exception e) {
-            e.printStackTrace();
+        // return false;//可行,不拦截事件,
+        // return true;//不行,孩子无法处理事件
+        //return super.onInterceptTouchEvent(ev);//不行,会有细微移动
+        if (isScroll) {
+            try {
+                return super.onInterceptTouchEvent(ev);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return false;
     }
 
+    /**
+     * 是否消费事件
+     * 消费:事件就结束
+     * 不消费:往父控件传
+     */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        try {
-            return isSlipping && super.onTouchEvent(ev);
-        } catch (Exception e) {
-            e.printStackTrace();
+        //return false;// 可行,不消费,传给父控件
+        //return true;// 可行,消费,拦截事件
+        //super.onTouchEvent(ev); //不行,
+        //虽然onInterceptTouchEvent中拦截了,
+        //但是如果viewpage里面子控件不是viewgroup,还是会调用这个方法.
+        if (isScroll) {
+            try {
+                return super.onTouchEvent(ev);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        return false;
+        return true;// 可行,消费,拦截事件
     }
 }
