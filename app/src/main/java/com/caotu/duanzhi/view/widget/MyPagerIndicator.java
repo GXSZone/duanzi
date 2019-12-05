471d48a03c6a57b63c906b1b4e59b09184a555ca
lwqiu <qlw2030@163.com>
2019-12-05 11:26:06 +0800

    修改首页指示器

--- app/src/main/java/com/caotu/duanzhi/view/widget/MyPagerIndicator.java
+++ app/src/main/java/com/caotu/duanzhi/view/widget/MyPagerIndicator.java
@@ -1,15 +1,15 @@
 package com.caotu.duanzhi.view.widget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.View;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 
-import com.caotu.duanzhi.R;
+import com.caotu.duanzhi.utils.DevicesUtils;
 
 import net.lucode.hackware.magicindicator.FragmentContainerHelper;
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
@@ -24,26 +24,28 @@ public class MyPagerIndicator extends View implements IPagerIndicator {
     private Interpolator mStartInterpolator = new LinearInterpolator();
     private Paint mPaint;
     private List<PositionData> mPositionDataList;
-    private Bitmap bitmap;
+    private RectF rectF;
+    private int tenInt;
+    private int sevenInt;
 
 
     public MyPagerIndicator(Context context) {
         super(context);
-        init(context);
+        init();
     }
 
-    private void init(Context context) {
+    private void init() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.FILL);
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nav_bg);
+        mPaint.setColor(Color.parseColor("#FE6785"));
+        rectF = new RectF();
+        tenInt = DevicesUtils.dp2px(10);
+        sevenInt = DevicesUtils.dp2px(7);
     }
-
-    float left;
-    float top;
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, left, top, mPaint);
+        canvas.drawRoundRect(rectF, 5, 5, mPaint);
     }
 
     @Override
@@ -58,13 +60,14 @@ public class MyPagerIndicator extends View implements IPagerIndicator {
 
         float leftX;
         float nextLeftX;
+        //默认有间距,需要稍微调整下
+        leftX = current.mContentRight - 10;
+        nextLeftX = next.mContentRight - 10;
 
-        leftX = current.mLeft + (current.width()) / 2.0f - bitmap.getWidth() / 2.0f;
-        nextLeftX = next.mLeft + (next.width()) / 2.0f- bitmap.getWidth() / 2.0f;
-
-        left = leftX + (nextLeftX - leftX) * mStartInterpolator.getInterpolation(positionOffset);
-        top = getHeight() / 2.0f - bitmap.getHeight() / 2.0f;
-
+        rectF.left = leftX + (nextLeftX - leftX) * mStartInterpolator.getInterpolation(positionOffset);
+        rectF.top = current.mContentBottom - tenInt;
+        rectF.right = rectF.left + sevenInt;
+        rectF.bottom = rectF.top + tenInt;
         invalidate();
     }
 
--- app/src/main/java/com/caotu/duanzhi/view/widget/ScaleTransitionPagerTitleView.java
+++ app/src/main/java/com/caotu/duanzhi/view/widget/ScaleTransitionPagerTitleView.java
@@ -6,6 +6,7 @@ import android.graphics.Typeface;
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
 
 public class ScaleTransitionPagerTitleView extends ColorTransitionPagerTitleView {
+    //值越大字体越大
     private float mMinScale = 0.80f;  //这个值来控制选中状态下和正常情况下的大小差异,外面设置的是最大值的情况
 
     public ScaleTransitionPagerTitleView(Context context) {
@@ -16,8 +17,8 @@ public class ScaleTransitionPagerTitleView extends ColorTransitionPagerTitleView
     public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
         setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
         super.onEnter(index, totalCount, enterPercent, leftToRight);    // 实现颜色渐变
-        setScaleX(mMinScale - 0.08f + (1.0f - mMinScale) * enterPercent);
-        setScaleY(mMinScale - 0.08f + (1.0f - mMinScale) * enterPercent);
+        setScaleX(mMinScale + 0.1f + (1.0f - mMinScale) * enterPercent);
+        setScaleY(mMinScale + 0.1f + (1.0f - mMinScale) * enterPercent);
     }
 
     @Override
@@ -28,11 +29,4 @@ public class ScaleTransitionPagerTitleView extends ColorTransitionPagerTitleView
         setScaleY(1.0f + (mMinScale - 1.0f) * leavePercent);
     }
 
-    public float getMinScale() {
-        return mMinScale;
-    }
-
-    public void setMinScale(float minScale) {
-        mMinScale = minScale;
-    }
 }
